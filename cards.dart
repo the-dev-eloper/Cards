3c071bc00a919f6cf1a2bdd83676c3e1aa1eae4b
the_dev.eloper <gokulnath@smazee.com>
2020-05-10 19:28:14 +0530

    init

---
+++ cards.dart
@@ -0,0 +1,36 @@
+void main() {
+  var deck = new Deck();
+
+  deck.removeCard('Diamonds', 'Ace');
+  print(deck);
+}
+
+class Deck() {
+
+  List<Card> cards = [];
+
+  Deck() {
+
+    var ranks = ['ace', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'king', 'queen', 'jack'];
+    var suits = ['Hearts', 'Diamonds', 'Spades', 'Clubs'];
+
+    for(var suit in suits) {
+
+      for(var rank in ranks) {
+
+        var card = new Card(rank, suit);
+        cards.add(card);
+      }
+    }
+  }
+
+  toString() {
+    return cards.toString();
+  }
+
+  shuffle() {
+    cards.shuffle();
+  }
+
+  cardsWithSuit(String suit) {}
+}
