b6e02c4ac3d3bc4af824429d70c71df7b76f04c2
the_dev.eloper <gokulnath@smazee.com>
2020-05-10 19:48:16 +0530

    init

--- cards.dart
+++ cards.dart
@@ -14,11 +14,15 @@ class Deck() {
     var ranks = ['ace', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'king', 'queen', 'jack'];
     var suits = ['Hearts', 'Diamonds', 'Spades', 'Clubs'];
 
-    for(var suit in suits) {
+    for(var mySuit in suits) {
 
       for(var rank in ranks) {
 
-        var card = new Card(rank, suit);
+        var card = new Card(
+          suit: mySuit,
+          rank: rank
+        );
+
         cards.add(card);
       }
     }
@@ -33,6 +37,31 @@ class Deck() {
   }
 
   cardsWithSuit(String suit) {
-    return card
+    return cards.where((card) => card.suit == suit);
+  }
+
+  deal(int handSize) {
+
+    var hand = cards.sublist(0, handSize);
+    cards = cards.sublist(handSize);
+
+    return hand;
+  }
+
+  removeCard(String suit, String rank) {
+
+    cards.removeWhere((card) => (card.suit == suit) && (card.rank == rank));
+  }
+}
+
+class Card() {
+
+  String suit;
+  String rank;
+
+  Card({this.rank, this.suit});
+
+  toString() {
+    return '$rank of $suit';
   }
 }
